5ee8ae5db9dad717296bf09d31d08cdf97558527
Damian Poole <damo@dlp.id.au>
2016-06-14 16:02:38 +1000

    dyl test

--- pam_tarpit.c
+++ pam_tarpit.c
@@ -7,8 +7,30 @@
 #include <unistd.h>
 #include <security/pam_appl.h>
 #include <security/pam_modules.h>
+#include <security/pam_ext.h>
 #include <libiptc/libiptc.h>
 #include <arpa/inet.h>
+#include <sys/utsname.h>
+
+#define TEXT_TO_DISPLAY \
+" ______________________________\n"\ 
+"/ Dear %s,                     \\\n"\ 
+"|                              |\n"\ 
+"| WHAT THE FUCK ARE YOU DOING? |\n"\ 
+"|                              |\n"\ 
+"\\ GET THE FUCK OUT!!!          /\n"\ 
+" ------------------------------\n"\ 
+"         \\\n"\ 
+"          \\\n"\ 
+"            ^__^ \n"\ 
+"    _______/(oo)\n"\ 
+"/\\/(       /(__)\n"\ 
+"   | W----|| |~|\n"\ 
+"   ||     || |~|  ~~\n"\ 
+"             |~|  ~\n"\ 
+"             |_| o\n"\ 
+"             |#|/\n"\ 
+"            _+#+_"
 
 void iptables_flush() {
 	struct xtc_handle *h;
@@ -43,7 +65,7 @@ void iptables_drop(const char *ip) {
       		entry.entry.ip.smsk.s_addr = 0xFFFFFFFF;
       	
  	     	/* add the rule */
- 	     	iptc_append_entry("INPUT", (struct ipt_entry *) &entry, h);
+ 	     	iptc_append_entry("SSH", (struct ipt_entry *) &entry, h);
       		iptc_commit(h);
       		iptc_free(h);
 		
@@ -60,28 +82,24 @@ PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const c
 
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
 	int retval;
+	char *resp = NULL;
+
 
 	const char* pUsername, *rhost;
-	retval = pam_get_user(pamh, &pUsername, "User: ");
-	
+        retval = pam_get_user(pamh, &pUsername, "User: ");
 
-	if (retval != PAM_SUCCESS) {
-		return retval;
-	}
-		
-	for(int i=0; i<argc; i++) {
-		if (strncmp(pUsername, argv[i], strlen(argv[i])) == 0) {
-			if(pam_get_item(pamh, PAM_RHOST, (const void **)&rhost) == PAM_SUCCESS) {
-				syslog(LOG_ALERT, "pam_tarpit: Tarpitting user %s from %s", argv[i], rhost);
-			}
-			//DROP host
-			iptables_drop(rhost);
-			sleep(666);
-			//flush INPUT chain
-			iptables_flush();
+        if (retval != PAM_SUCCESS) {
+                return retval;
+        }
+	if(pam_get_item(pamh, PAM_RHOST, (const void **)&rhost) == PAM_SUCCESS) {
+		syslog(LOG_ALERT, "pam_tarpit: Tarpitting user %s from %s", pUsername, rhost);
+		retval = pam_info(pamh, TEXT_TO_DISPLAY, rhost);
+		if(retval != PAM_CONV_ERR) {
 			return PAM_AUTH_ERR;
 		}
+		iptables_drop(rhost);
+		sleep(666);
 	}
-	
+
 	return PAM_AUTH_ERR;
 }
